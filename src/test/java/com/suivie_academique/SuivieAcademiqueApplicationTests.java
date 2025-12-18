2750b718d2efb5e99f7c9003f69bc24b50ea4525
RUBEN MAGDIEL DJAKOU TCHUETKA <djakoumagdiel@gmail.com>
2025-12-18 14:36:41 +0100

    correction de suiviaSuivieAcademiqueApplicationTests

--- src/test/java/com/suivie_academique/SuivieAcademiqueApplicationTests.java
+++ src/test/java/com/suivie_academique/SuivieAcademiqueApplicationTests.java
@@ -1,9 +1,11 @@
 package com.suivie_academique;
 
+import com.suivi_academique.SuiviAcademiqueApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SuiviAcademiqueApplication.class)
+
 class SuivieAcademiqueApplicationTests {
 
 	@Test
